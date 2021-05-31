c4b6ba98eed21322be21c354586c9e289ad38669
zusuyu <zusuyu@stu.pku.edu.cn>
2021-05-31 10:36:41 +0800

    5.31 commit

--- main.py
+++ main.py
@@ -126,33 +126,38 @@ def getComboMask(combo):
 
 
 
+
+
+_input_size = 0
+
 class Game(object):
     # 这里 0 始终是地主，1 始终是地主下家，2 始终是地主上家
 
     def __init__(self, init_data):
-        self.hand = [np.zeros(15, dtype = int), np.zeros(15, dtype = int), np.zeros(15, dtype = int)]
+        self.hand = np.zeros((3, 15), dtype = int)
         for player in range(3):
             for card in init_data[player]:
-                self.hand[player][getCardId(card)] += 1
+                self.hand[player, getCardId(card)] += 1
+        self.initial_hand = self.hand.copy()
     
     def play(self, player, cards):
         # 模拟打牌 打出 cards 这个 list 中的所有牌
         for card in cards:
-            self.hand[player][getCardId(card)] -= 1
+            self.hand[player, getCardId(card)] -= 1
             
     def possess(self, player, combo):
         # 判断 player 这个玩家是否拥有 combo 这个牌型的牌
         if combo == (0, 0, 0, 0):
             return True
         for i in range(combo[1], combo[2] + 1):
-            if self.hand[player][i] < combo[0]:
+            if self.hand[player, i] < combo[0]:
                 return False
             
         fold = [0, 0, 0, 1, 2]
         need_wings = (combo[2] - combo[1] + 1) * fold[combo[0]] if combo[3] > 0 else 0
         for i in range(15):
             if i < combo[1] or i > combo[2]:
-                if self.hand[player][i] >= combo[3]:
+                if self.hand[player, i] >= combo[3]:
                     need_wings -= 1
         if need_wings > 0:
             return False
@@ -176,42 +181,73 @@ class Game(object):
         # 指明 combo 后：(1)少于1/2张的不能打 (2)和主体部分重复的不能打 (3)打过的不能打
         mask = np.ones(15 if combo[3] == 1 else 13)
         for i in range(mask.shape[0]):
-                if self.hand[player][i] < combo[3]:
+                if self.hand[player, i] < combo[3]:
                     mask[i] = 0
         mask[range(combo[1], combo[2] + 1)] = 0
         mask[already_played] = 0
         return mask
-    
-    def getInput(self, player):
-        # 返回两个网络的输入
-        # 这里包含五个部分：我自己每种牌的数量、对手每种牌的数量、我的顺子情况、三个人还剩多少张牌、我拥有牌型的 mask
-        # size = 4 * 15 + 4 * 15 + 4 * 12 + 3 * 20 + 379
+        
+    def getInput(self, player, combos):
+        global _input_size
+        
         p1 = (player + 1) % 3
         p2 = (player + 2) % 3
-
+        '''
         myhand = np.zeros((4, 15))
         othershand = np.zeros((4, 15))
         for i in range(4):
             myhand[i, np.where(self.hand[player] == i + 1)] = 1
             othershand[i, np.where(self.hand[p1] + self.hand[p2] == i + 1)] = 1
         
-        mystraight = np.zeros((4, 12))
-        for i in range(4):
-            k = 0
-            for j in range(12):
-                if self.hand[player][i] >= i + 1:
-                    k += 1
-                else:
-                    k = 0
-                mystraight[i, j] = k
+        played_cards = np.zeros((3, 4, 15))
+        p_list = [player, p1, p2]
+        for i in range(3):
+            p = p_list[i]
+            for j in range(4):
+                played_cards[i, j, np.where(self.initial_hand[p] - self.hand[p] == j + 1)] = 1
                 
         handcnt = np.zeros((3, 20))
         for player in range(3):
-            handcnt[player, np.sum(self.hand[player]) - 1] = 1
+            handcnt[player, range(np.sum(self.hand[player]))] = 1
+        
+        Input = np.concatenate([myhand.flatten(),
+                               othershand.flatten(),
+                               played_cards.flatten(),
+                               handcnt.flatten()
+#                               self.getPossessMask(player)
+                              ])
+        '''
+        # 我手里有每种数值的牌多少张，对手还有每种数值的牌多少张没有出过
+        Input = np.concatenate([np.array(self.hand[player]),
+                                np.array(self.hand[p1]) + np.array(self.hand[p2])])
+        # 我们三个人各打过每个数值多少张牌，各还有多少张牌
+        for p in range(3):
+            Input = np.concatenate([Input,
+                                    np.array(self.initial_hand[p]) - np.array(self.hand[p]),
+                                    np.array([np.sum(self.hand[p])])
+                                   ])
+        def getComboArray(combo):
+            tmp = np.zeros(15)
+            for i in range(combo[1], combo[2] + 1):
+                tmp[i] += combo[0]
+            return tmp
+        # 前两回合那两个人各出了什么牌型的牌（只记录主体）
+        for i in range(1, 3):
+            Input = np.concatenate([Input,
+                                    getComboArray(combos[(player + i) % 3])
+                                   ])
+        # 我还有的牌型
+        Input = np.concatenate([Input, self.getPossessMask(player)])
+        # 我的手牌的差分(12张可连顺的部分)
+        tmp = np.zeros(11)
+        for i in range(11):
+            tmp[i] = self.hand[player, i + 1] - self.hand[player, i]
+        Input = np.concatenate([Input, tmp])
+        _input_size = Input.shape[0]
+        return Input
 
-        return np.concatenate([myhand.flatten(), othershand.flatten(), mystraight.flatten(), handcnt.flatten(), self.getPossessMask(player)])
-    
-_input_size = 60 + 60 + 48 + 60 + combo_cnt
+
+
 
 
 
@@ -230,14 +266,14 @@ class MyModule(nn.Module):
         self.fc = nn.Sequential(OrderedDict([
                 ('fc1', nn.Linear(INPUT_SIZE, HIDDEN_SIZE)),
                 ('relu', nn.ReLU()),
+#                ('bn', nn.BatchNorm1d(HIDDEN_SIZE)),
                 ('dropout', nn.Dropout(p = 0.2)),
                 ('fc2', nn.Linear(HIDDEN_SIZE, OUTPUT_SIZE))
             ]))
         
     def forward(self, x, m):
-        return nn.LogSoftmax(dim = -1)(self.fc(x) * m)
-    
-    
+        return self.fc(x) * m
+        
     
     
     
@@ -252,6 +288,7 @@ my_hand = []
 g = Game([[], [], []])
 my_pos = -1
 others = []
+combos = [(0, 0, 0, 0), (0, 0, 0, 0), (0, 0, 0, 0)]
 las_combo = (0, 0, 0, 0)
 
 def BIDDING():
@@ -274,17 +311,16 @@ def PLAYING():
                 return c
     
     to_play = []
-    
+        
     model_name = "best_model_for_" + str(my_pos) + "mainbody.pt"
-    model = MyModule(_input_size, combo_cnt)
-    model.load_state_dict(torch.load(model_path + model_name))
+    model = torch.load(model_path + model_name)
     
     mask = g.getMask1(my_pos, las_combo)
     combo_id = -1
     if np.sum(mask) == 1:
         combo_id = np.argmax(mask)
     else:
-        combo_id = model(torch.from_numpy(g.getInput(my_pos)).unsqueeze(0),
+        combo_id = model(torch.from_numpy(g.getInput(my_pos, combos)).unsqueeze(0),
                          torch.from_numpy(mask)).unsqueeze(0).detach().numpy().argmax()
     
     combo = combo_list[combo_id]
@@ -295,13 +331,12 @@ def PLAYING():
     
     if combo[3] != 0:
         model_name = "best_model_for_" + str(combo[3] - 1) + "bywings.pt"
-        model = MyModule(_input_size, (15 if combo[3] == 1 else 13))
-        model.load_state_dict(torch.load(model_path + model_name))
+        model = torch.load(model_path + model_name)
 
         cnt = (combo[2] - combo[1] + 1) * (1 if combo[0] == 3 else 2)
         already_played = []
         for i in range(cnt):
-            wing_id = model(torch.from_numpy(g.getInput(my_pos)),
+            wing_id = model(torch.from_numpy(g.getInput(my_pos, combos)),
                             torch.from_numpy(g.getMask2(my_pos, combo, already_played))).detach().numpy().argmax()
             tmp = []
             if wing_id < 15:
@@ -358,17 +393,19 @@ if __name__ == "__main__":
                 p = others[j]
                 cards = history[j]
                 g.play(p, cards)
-                cur_combo = getCombo(cards)
-                if cur_combo != (0, 0, 0, 0):
-                    las_combo = cur_combo
+                combos[p] = getCombo(cards)
 
             if i < len(data["requests"]) - 1:
                 cards = data["responses"][i]
                 g.play(my_pos, cards)
                 for c in cards:
                     my_hand.remove(c)
-                las_combo = (0, 0, 0, 0)
-        
+                combos[my_pos] = getCombo(cards)
+    
+    for i in range(1, 3):
+        if combos[(my_pos + i) % 3] != (0, 0, 0, 0):
+            las_combo = combos[(my_pos + i) % 3]
+
     if TODO == "bidding":
         BIDDING()
     else:
